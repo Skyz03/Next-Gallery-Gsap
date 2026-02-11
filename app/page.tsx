aee8d75be5a7e862b26e9b78ef726b8e012f558f
Skyz03 <aakibdiscoo@gmail.com>
2026-02-11 06:53:47 +0545

    fix: update with scroll

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,36 @@ export default function Home() {
       <div className="lg:col-span-4">
         <ProjectCard image="/wed2.jpg" title="Ethereal Grace" location="Tuscany" aspect="portrait" />
       </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed3.jpg" title="Moments De Luxe" location="Catelo" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed4.jpg" title="Graceful Moments" location="Boracay" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed5.jpg" title="Tuscany" location="Fanteko" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed6.jpg" title="Mystical Moments" location="Valino" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed7.jpg" title="Scorching Moments" location="Botesaka" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed8.jpg" title="Find Your Perfect Moment" location="Boadesa" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed3.jpg" title="Gathering De Luxe" location="Dumess" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed10.jpg" title="Dane De Cate" location="Rackeo" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed11.jpg" title="Ethereal Di Fout" location="Barbero" aspect="portrait" />
+      </div>
+      <div className="lg:col-span-4">
+        <ProjectCard image="/wed12.jpg" title="Mixining Moments" location="Pluta" aspect="portrait" />
+      </div>
     </section>
   );
 }
